ae24ebcb1538ad0f6f1cb20964ee31c93bb8a6bb
grassl <johannes.grassl@sabel.education>
2025-10-27 09:22:03 +0100

    basic structure

--- core/src/main/java/de/gbs/projekt/Main.java
+++ core/src/main/java/de/gbs/projekt/Main.java
@@ -1,32 +1,63 @@
 package de.gbs.projekt;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
+    private FitViewport viewport;
     private Texture image;
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true); // true centers the camera
+    }
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        viewport = new FitViewport(80,60);
         image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        ScreenUtils.clear(0, 0, 0, 1);
+        input();
+        logic();
+        draw();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
         image.dispose();
+    }
+
+    private void input() {
+        float delta = Gdx.graphics.getDeltaTime();
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        batch.begin();
+
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        batch.draw(image, 0, 0, worldWidth, worldHeight);
+        batch.end();
     }
 }
