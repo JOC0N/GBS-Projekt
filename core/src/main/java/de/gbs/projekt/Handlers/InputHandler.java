96fd52ad66e4c43a1cd7a809759f3b0778caa16d
JOC0N <joflo1@gmx.de>
2025-10-30 09:51:44 +0100

    input handler update

--- core/src/main/java/de/gbs/projekt/Handlers/InputHandler.java
+++ core/src/main/java/de/gbs/projekt/Handlers/InputHandler.java
@@ -1,21 +1,43 @@
 package de.gbs.projekt.Handlers;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import de.gbs.projekt.objects.Player;
 
 public class InputHandler {
-    private float delta;
+    private Player player;
+    private float speed = 200; // Bewegungsgeschwindigkeit in Pixeln pro Sekunde
 
-    public InputHandler() {
-        delta = Gdx.graphics.getDeltaTime();
+    public void setPlayer(Player player) {
+        this.player = player;
     }
 
-    public void run(){
-        delta = Gdx.graphics.getDeltaTime();
+    public void run() {
+        if (player == null) return;
 
+        float velocityX = 0;
+        float velocityY = 0;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            velocityX = -1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            velocityX = 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            velocityY = 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            velocityY = -1;
+        }
+
+        // Normalisierung für diagonale Bewegung (optional)
+        if (velocityX != 0 && velocityY != 0) {
+            velocityX *= 0.7071F; // 1 / sqrt(2)
+            velocityY *= 0.7071F;
+        }
+
+        player.setX(player.getX() + velocityX * speed * Gdx.graphics.getDeltaTime());
+        player.setY(player.getY() + velocityY * speed * Gdx.graphics.getDeltaTime());
     }
-
-    public float getDelta() {
-        return delta;
-    }
-
 }
