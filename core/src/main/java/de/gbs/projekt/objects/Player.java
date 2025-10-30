be60c917bdb756765c3520d1094e79dc5f4e5ca8
JOC0N <joflo1@gmx.de>
2025-10-30 10:35:41 +0100

    game objects close call

--- core/src/main/java/de/gbs/projekt/objects/Player.java
+++ core/src/main/java/de/gbs/projekt/objects/Player.java
@@ -14,6 +14,7 @@ public class Player extends GameObject {
     @Override
     public void update(float delta) {
         // Spieler-Logik aktualisieren (z. B. Bewegung)
+        x = x +1;
     }
 
     @Override
